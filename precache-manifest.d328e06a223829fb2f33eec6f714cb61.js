15c234170f3d142679252ef64d9a6d36cc63dbe0
KTELLAWI <koutaiba@msn.com>
2021-01-02 22:20:00 +0300

    Updates

---
+++ precache-manifest.d328e06a223829fb2f33eec6f714cb61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809460ccf779d81d361160bacdac9867",
+    "url": "/redux/index.html"
+  },
+  {
+    "revision": "c7c0410d66fc3bbef0a3",
+    "url": "/redux/static/css/2.4195bf35.chunk.css"
+  },
+  {
+    "revision": "79a1bf6b01ba4e7a6eb7",
+    "url": "/redux/static/css/main.aee38e1c.chunk.css"
+  },
+  {
+    "revision": "c7c0410d66fc3bbef0a3",
+    "url": "/redux/static/js/2.b8188944.chunk.js"
+  },
+  {
+    "revision": "79a1bf6b01ba4e7a6eb7",
+    "url": "/redux/static/js/main.d22be5f3.chunk.js"
+  },
+  {
+    "revision": "f51989da7e50da5ac116",
+    "url": "/redux/static/js/runtime~main.e9904234.js"
+  },
+  {
+    "revision": "2c51f81ea0f98a3238ca69bda8990a5f",
+    "url": "/redux/static/media/reminder.2c51f81e.png"
+  }
+]);
